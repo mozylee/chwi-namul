0cf1c20869036e544f4d91a57060c0c1d2241136
mozylee <whyalwaysmeyy@gmail.com>
2023-10-14 15:20:06 +0900

    feat: 크롤링 후 insert 전환

--- src/main/kotlin/com/toy/chwinamul/crawler/service/impl/CrawlingServiceImpl.kt
+++ src/main/kotlin/com/toy/chwinamul/crawler/service/impl/CrawlingServiceImpl.kt
@@ -1,23 +1,35 @@
 package com.toy.chwinamul.crawler.service.impl
 
 import com.toy.chwinamul.common.utils.CrawlingUtil
-import com.toy.chwinamul.crawler.dto.JobDescription
+import com.toy.chwinamul.crawler.domain.JobDescription
+import com.toy.chwinamul.crawler.domain.JobDescriptionRepository
 import com.toy.chwinamul.crawler.dto.KakaoBankRecruitment
 import com.toy.chwinamul.crawler.service.CrawlingService
 import org.openqa.selenium.By
+import org.openqa.selenium.chrome.ChromeDriver
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 private const val URL = "https://recruit.kakaobank.com/jobs"
 
 @Service
-class CrawlingServiceImpl() : CrawlingService {
+class CrawlingServiceImpl(
+    private val jobDescriptionRepository: JobDescriptionRepository
+) : CrawlingService {
 
-    override fun crawlKakaoBank(): List<JobDescription> = CrawlingUtil.crawl { driver ->
+    @Transactional
+    override fun crawlKakaoBank(): List<JobDescription> {
+        val jobDescriptions = CrawlingUtil.crawl(::findJobDescriptions)
+
+        return jobDescriptionRepository.saveAll(jobDescriptions).toList()
+    }
+
+    private fun findJobDescriptions(driver: ChromeDriver): List<JobDescription> {
         driver.get(URL)
 
         Thread.sleep(3000);
 
-        driver.findElements(By.className("link_tit"))
+        return driver.findElements(By.className("link_tit"))
             .map(::KakaoBankRecruitment)
             .map(KakaoBankRecruitment::toJobDescription)
             .toList()
